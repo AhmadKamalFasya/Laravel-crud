1d9dc5e10dc46cc221d2def45e2ecf09b231a130
Ahmad Kamal Fasya <fasyaaoi9@gmail.com>
2021-02-20 10:40:56 +0700

    Memodifikasi routes home dan v_template.blade.php

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -57,7 +57,7 @@
             <!-- End Navbar -->
             <div class="content">
                 <div class="container-fluid">
-                    <!-- your content here -->
+                    @yield('content')
                 </div>
             </div>
             <footer class="footer">
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout.v_template');
+    return view('v_home');
 });
