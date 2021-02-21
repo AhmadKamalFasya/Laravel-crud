0584be79fa8b0b35bf7bfe3478ceb15d7972e179
Ahmad Kamal Fasya <fasyaaoi9@gmail.com>
2021-02-22 06:25:38 +0700

    Mengubah tombol edit di file v_mahasiswa_blade.php

--- resources/views/v_mahasiswa.blade.php
+++ resources/views/v_mahasiswa.blade.php
@@ -32,7 +32,7 @@
                                         <td>{{$data->prodi}}</td>
                                         <td>{{$data->mata_kuliah}}</td>
                                         <td>
-                                            <a href="" class="badge badge-primary">Detail</a>
+                                            <a href="/mahasiswa/detail/{{$data->mahasiswa_id}}" class="badge badge-primary">Detail</a>
                                             <a href="" class="badge badge-success">Edit</a>
                                             <a href="" class="badge badge-danger">Delete</a>
                                         </td>
